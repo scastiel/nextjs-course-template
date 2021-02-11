1fe9dbcc3ef1290fa250227318130565848b173e
Sebastien Castiel <sebastien@castiel.me>
2021-02-10 19:23:47 -0500

    Add content on home page

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,52 @@ const Home = ({ lesson }) => {
   return (
     <Layout widthClass="max-w-4xl">
       <div className="flex flex-col items-center mb-16">
-        <div
-          id="preview"
-          data-aos="fade"
-          className="flex flex-col self-stretch"
-        >
+        <div className="flex flex-col self-stretch">
+          <p className="text-center text-4xl mt-32 font-bold">
+            This is the demo of a{' '}
+            <strong className="text-pink-800 font-bold">template</strong> you
+            <br className="hidden sm:inline" /> can use to create{' '}
+            <strong className="text-pink-800 font-bold">
+              courses with Next.js
+            </strong>
+            .
+          </p>
+        </div>
+        <div className="text-center mt-8">
+          <a href="/course" className="btn btn-primary btn-big">
+            Go to the course
+          </a>
+        </div>
+
+        <div data-aos="fade" className="flex flex-col self-stretch">
+          <p className="text-center text-4xl mt-32 font-bold">
+            It‘s open source!
+          </p>
+
+          <p className="text-center text-2xl mt-6 text-gray-500 leading-8">
+            I created this template for my{' '}
+            <a href="https://useeffect.dev" className="text-pink-800">
+              course on React hooks
+            </a>
+            .
+          </p>
+          <p className="text-center text-2xl mt-3 text-gray-500 leading-8">
+            You’ll find on the GitHub repository the information you’ll
+            <br className="hidden sm:inline" />
+            need if you want to use it, or if you are just curious 😉
+          </p>
+
+          <div className="text-center mt-8">
+            <a
+              href="https://github.com/scastiel/nextjs-course-template"
+              className="btn btn-primary btn-big"
+            >
+              Go to the GitHub repo
+            </a>
+          </div>
+        </div>
+
+        <div className="flex flex-col self-stretch">
           <p className="text-center text-4xl mt-32 font-bold">
             You can embed a{' '}
             <strong className="text-pink-800 font-bold">preview</strong> on the
